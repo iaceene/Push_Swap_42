6c7f6bb41cd309ae75cd0c03ea734499e0ac3e06
iaceene <yassineajagrou986@gmail.com>
2024-12-17 18:36:00 +0100

    fix bugs

--- bonus/checker_bonus.c
+++ bonus/checker_bonus.c
@@ -6,26 +6,22 @@
 /*   By: yaajagro <yaajagro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 09:23:57 by yaajagro          #+#    #+#             */
-/*   Updated: 2024/12/17 11:33:51 by yaajagro         ###   ########.fr       */
+/*   Updated: 2024/12/17 18:35:14 by yaajagro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap_bonus.h"
 #include "get_next_line_bonus.h"
 
-int	ft_strncmp(const char *str1, const char *str2, size_t n)
+int	ft_strcmp(char *str1, char *str2)
 {
-	size_t	i;
+	int	i;
 
-	if (n == 0)
-		return (0);
 	i = 0;
-	while (i < n)
+	while (str1[i])
 	{
 		if (str1[i] != str2[i])
 			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
-		if (str1[i] == '\0')
-			return (0);
 		i++;
 	}
 	return (0);
@@ -33,27 +29,27 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 
 int	instruction_valid(char *buffer, t_list **stack_a, t_list **stack_b)
 {
-	if (ft_strncmp(buffer, "sa", ft_strlen(buffer) - 1) == 0)
+	if (ft_strcmp(buffer, "sa\n") == 0)
 		ft_swap_stack(stack_a);
-	else if (ft_strncmp(buffer, "sb", ft_strlen(buffer) - 1) == 0)
+	else if (ft_strcmp(buffer, "sb\n") == 0)
 		ft_swap_stack(stack_b);
-	else if (ft_strncmp(buffer, "ss", ft_strlen(buffer) - 1) == 0)
+	else if (ft_strcmp(buffer, "ss\n") == 0)
 		ft_swap_all(stack_a, stack_b);
-	else if (ft_strncmp(buffer, "pa", ft_strlen(buffer) - 1) == 0)
+	else if (ft_strcmp(buffer, "pa\n") == 0)
 		ft_push(stack_b, stack_a);
-	else if (ft_strncmp(buffer, "pb", ft_strlen(buffer) - 1) == 0)
+	else if (ft_strcmp(buffer, "pb\n") == 0)
 		ft_push(stack_a, stack_b);
-	else if (ft_strncmp(buffer, "ra", ft_strlen(buffer) - 1) == 0)
+	else if (ft_strcmp(buffer, "ra\n") == 0)
 		ft_rotate(stack_a);
-	else if (ft_strncmp(buffer, "rb", ft_strlen(buffer) - 1) == 0)
+	else if (ft_strcmp(buffer, "rb\n") == 0)
 		ft_rotate(stack_b);
-	else if (ft_strncmp(buffer, "rr", ft_strlen(buffer) - 1) == 0)
+	else if (ft_strcmp(buffer, "rr\n") == 0)
 		ft_rotate_all(stack_a, stack_b);
-	else if (ft_strncmp(buffer, "rra", ft_strlen(buffer) - 1) == 0)
+	else if (ft_strcmp(buffer, "rra\n") == 0)
 		ft_reverse_rotate(stack_a);
-	else if (ft_strncmp(buffer, "rrb", ft_strlen(buffer) - 1) == 0)
+	else if (ft_strcmp(buffer, "rrb\n") == 0)
 		ft_reverse_rotate(stack_b);
-	else if (ft_strncmp(buffer, "rrr", ft_strlen(buffer) - 1) == 0)
+	else if (ft_strcmp(buffer, "rrr\n") == 0)
 		ft_reverse_all(stack_a, stack_b);
 	else
 		return (1);
